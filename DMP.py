b48312d4c501ee9e50f90e980dc820f5138cc5e9
hgirgin <hakangirgin21@gmail.com>
2018-01-19 10:34:44 +0300

    Learning from multiple demos added.

--- DMP.py
+++ DMP.py
@@ -1,18 +1,18 @@
 # Copyright (C) 2017 CoLoRs-AILAB, Bogazici University
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
+#
 # Authors : Hakan Girgin <hakangirgin21@gmail.com>
 
 
@@ -20,8 +20,8 @@ import numpy as np
 
 class DMP:
 
-	def __init__(self, BF_number, K, X):
-
+	def __init__(self, BF_number, K, task_number X):
+		self.task_number = task_number
 		self.BF_number   = BF_number 		 # Basis function number
 		self.t 	         = X[:, 0] - X[0, 0] # Making the time start from 0s.
 		self.x 			 = X[:, 1:]	         # Demonstrated trajectory
@@ -37,7 +37,11 @@ class DMP:
 
 		convergence_rate = 0.001
 		self.alpha       = -np.log(convergence_rate)
-		self.s           = np.exp(((-self.alpha/self.tau)*self.t))
+		if self.task_number == 1:
+			self.s           = np.exp(((-self.alpha/self.tau)*self.t))
+		else:
+			self.s		= np.exp(((-self.alpha/self.tau)*self.t[:self.dataLength/self.task_number]))
+			self.s           = np.tile(self.s,(self.task_number,))
 
 		# Creating basis functions and psiMatrix
 		self.sumBFs, self.psiMatrix = self.getBasisFunctions()
@@ -52,7 +56,7 @@ class DMP:
 		self.c    = np.logspace(-3,0,num=self.BF_number ) # centers of basis functions
 		self.h    = self.BF_number/ (self.c**2)           # widths of basis functions
 
-		psi  = np.zeros(self.dataLength)  
+		psi  = np.zeros(self.dataLength)
 		for i in range(0,self.BF_number):
 			psi = np.column_stack( [psi, np.exp(-self.h[i]*((self.s-self.c[i])**2))] )
 		psiMatrix = psi[:,1:]
@@ -63,12 +67,19 @@ class DMP:
 
 	def learnWeights(self):
 
-		x0        = self.x[0,:]          # initial position
-		g         = self.x[-1,:]	     # goal position
+	f_target_holder = []
+	for k in range(self.task_number):
+		i = range(k*self.dataLength/self.task_number, (k+1)*self.dataLength/self.task_number)
+		x = self.x[i]
+		t = self.t[i]
+		dataLength = self.dataLength/self.task_number
+
+		x0        = x[0,:]          # initial position
+		g         = x[-1,:]	     # goal position
 
 		# Numerical Differentiations
-		dt        = np.transpose(np.broadcast_to(np.diff(self.t),(self.DoF,self.dataLength-1)))
-		dx        = np.diff(self.x,axis = 0)
+		dt        = np.transpose(np.broadcast_to(np.diff(t),(self.DoF,dataLength-1)))
+		dx        = np.diff(x,axis = 0)
 
 		x_dot     = np.row_stack((np.zeros(self.DoF),dx/dt))
 		dx_dot    = np.diff(x_dot, axis=0)
@@ -78,20 +89,23 @@ class DMP:
 		v_dot     = self.tau*x_dotdot
 
 		# Finding the target nonlinear function for each time step
-		f_target = np.zeros(self.DoF) 
-		for i in range(self.dataLength):
+		f_target = np.zeros(self.DoF)
+		for i in range(dataLength):
 			f_temp = np.dot(np.linalg.inv(self.K), self.tau*v_dot[i,:] + \
-				np.dot(self.D,v[i,:]) )  + (self.x[i,:]-g) + self.s[i]*(g-x0)
+				np.dot(self.D,v[i,:]) )  + (x[i,:]-g) + self.s[i]*(g-x0)
 
 			f_target = np.column_stack((f_target,f_temp))
 
 		f_target = np.transpose(f_target)
 		f_target = f_target[1:,:]
-
-		# Linear Least Squares Regression
-		X       = np.transpose(np.transpose(self.psiMatrix) * self.s/self.sumBFs)
-		w_temp  = np.linalg.inv(np.dot(np.transpose(X),X))
-		weights = np.dot(w_temp,np.dot(np.transpose(X),f_target))
+		f_target_holder.append(f_target)
+		
+	ft = np.array(f_target_holder)
+	ft = np.reshape(ft, (self.dataLength,ft.shape[2]))
+	# Linear Least Squares Regression
+	X       = np.transpose(np.transpose(self.psiMatrix) * self.s/self.sumBFs)
+	w_temp  = np.linalg.inv(np.dot(np.transpose(X),X))
+	weights = np.dot(w_temp,np.dot(np.transpose(X),ft))
 
 		return weights
 

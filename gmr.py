295ef5064899cc225b36d07dd6c31969caf50583
serkanbugur <serkanbugur@gmail.com>
2018-07-16 12:25:09 +0300

    Indentation corrections are made

--- gmr.py
+++ gmr.py
@@ -2,39 +2,38 @@
 import numpy as np
 from scipy.stats import multivariate_normal
 
+
 def gmr(X, means, covariances):
     EPSILON = 1
-    K       = means.shape[0] # number of hidden states
-    OUT     = means.shape[1] # number of dimensions in input+output
+    K = means.shape[0]  # number of hidden states
+    OUT = means.shape[1]  # number of dimensions in input+output
 
     if type(X) == int or type(X) == float or type(X) == np.float64:
         DATA = 1
-        INP  = 1 ;
-    else :
+        INP = 1;
+    else:
         DATA = X.shape[0]
-        INP  = X.shape[1]
+        INP = X.shape[1]
 
-    input_  = X
+    input_ = X
 
-    #Finding sum of hi
+    # Finding sum of hi
     sum_all = []
-    for datapoint in range(0,DATA):
+    for datapoint in range(0, DATA):
 
         if type(input_) == int or type(input_) == float or type(X) == np.float64:
-            x           = input_
+            x = input_
         else:
-            x           = input_[datapoint,:]
+            x = input_[datapoint, :]
 
         sum_of_datapoint = 0
-        for gaussian in range(0,K):
+        for gaussian in range(0, K):
 
             mean_i = means[gaussian]
             mean_x = mean_i[0:INP]
 
-            covars_i                = covariances[gaussian]
-            covars_xx               = covars_i[0:INP , 0:INP]
-
-
+            covars_i = covariances[gaussian]
+            covars_xx = covars_i[0:INP, 0:INP]
 
             if len(mean_x) > 1:
                 h_datapoint = multivariate_normal.pdf(x, mean_x, EPSILON * covars_xx, allow_singular=True)
@@ -43,52 +42,47 @@ def gmr(X, means, covariances):
                 covars_xx = covars_xx[0]
                 h_datapoint = multivariate_normal.pdf(x, mean_x, EPSILON * covars_xx, allow_singular=True)
 
-            sum_of_datapoint        = sum_of_datapoint + h_datapoint
-
+            sum_of_datapoint = sum_of_datapoint + h_datapoint
 
         sum_all.append(sum_of_datapoint)
 
-    #Gaussian Mixture Regression
-    output = np.zeros(OUT-INP)
-    for datapoint in range(0,DATA):
+    # Gaussian Mixture Regression
+    output = np.zeros(OUT - INP)
+    for datapoint in range(0, DATA):
 
         if type(input_) == int or type(input_) == float or type(X) == np.float64:
-            x           = input_
+            x = input_
         else:
-            x           = input_[datapoint,:]
+            x = input_[datapoint, :]
 
-        output_datapoint        = np.zeros(OUT-INP)
-        for gaussian in range(0,K):
+        output_datapoint = np.zeros(OUT - INP)
+        for gaussian in range(0, K):
 
-            mean_i         = means[gaussian]
-            mean_x         = mean_i[0:INP]
-            mean_y         = mean_i[INP:OUT]
+            mean_i = means[gaussian]
+            mean_x = mean_i[0:INP]
+            mean_y = mean_i[INP:OUT]
 
-            covars_i       = covariances[gaussian]
-            covars_xx      = covars_i[0:INP , 0:INP]
-            covars_yx      = covars_i[INP:OUT, 0:INP]
+            covars_i = covariances[gaussian]
+            covars_xx = covars_i[0:INP, 0:INP]
+            covars_yx = covars_i[INP:OUT, 0:INP]
 
+            if len(mean_x) > 1:  # if MIMO and MISO
 
-            if len(mean_x) > 1: # if MIMO and MISO
+                cov = np.dot(covars_yx, np.linalg.inv(covars_xx))
 
-                cov      = np.dot( covars_yx, np.linalg.inv(covars_xx) )
+                inside_i = mean_y + np.dot(cov, x - mean_x)
+                h_i = multivariate_normal.pdf(x, mean_x, EPSILON * covars_xx, allow_singular=True)
 
-                inside_i = mean_y + np.dot( cov, x-mean_x )
-                h_i      = multivariate_normal.pdf(x, mean_x, EPSILON * covars_xx, allow_singular=True)
-
-            elif len(mean_x) == 1: # if SIMO and SISO
+            elif len(mean_x) == 1:  # if SIMO and SISO
                 covars_xx = covars_xx[0]
-                inside_i  = mean_y + np.dot( covars_yx ,(x - mean_x)/covars_xx )
+                inside_i = mean_y + np.dot(covars_yx, (x - mean_x) / covars_xx)
 
                 h_i = multivariate_normal.pdf(x, mean_x, EPSILON * covars_xx, allow_singular=True)
 
-            h_i            = h_i / sum_all[datapoint]
-
-
+            h_i = h_i / sum_all[datapoint]
 
             output_datapoint = output_datapoint + h_i * inside_i
 
-
         output = np.row_stack([output, output_datapoint])
         # print output
-    return output[1:,:]
+    return output[1:, :]
--- hmm.py
+++ hmm.py
@@ -5,243 +5,218 @@ import numpy.testing as npt
 from sklearn.cluster import KMeans
 from scipy.misc import logsumexp
 from scipy.stats import multivariate_normal
+
 np.set_printoptions(precision=3)
 
+
 class hmm:
+    def __init__(self, number_of_hidden_states, training_data):
 
-	def __init__(self,number_of_hidden_states, training_data):
+        self.n = number_of_hidden_states
+        self.data = training_data
+        self.dataLength = self.data.shape[0]
+        self.dataDimens = self.data.shape[1]
 
-		self.n          = number_of_hidden_states
-		self.data       = training_data
-		self.dataLength = self.data.shape[0]
-		self.dataDimens = self.data.shape[1]
+    def initPrior(self):
+        # Uniform Initialization of Priors
+        prior = np.ones(self.n) * (1.0 / self.n)
+        return np.log(prior)
 
+    def initTransition(self):
 
-	def initPrior(self):
-		# Uniform Initialization of Priors
-		prior = np.ones(self.n)*(1.0/self.n)
-		return np.log(prior)
+        # Uniform Initialization of Transition Matrix
+        a = np.ones((self.n, self.n)) * (1.0 / self.n)
 
-	def initTransition(self):
+        return np.log(a)
 
-		# Uniform Initialization of Transition Matrix
-		a = np.ones((self.n,self.n))*(1.0/self.n)
+    def initMeansCovs(self):
 
-		return np.log(a)
+        kmeans = KMeans(n_clusters=self.n, random_state=0).fit(self.data)
+        mu = kmeans.cluster_centers_
+        labels = kmeans.labels_
+        mu = np.array(sorted(mu.tolist()))
+        labels = np.array(sorted(labels.tolist()))
 
-	def initMeansCovs(self):
-		
-		kmeans = KMeans(n_clusters=self.n, random_state=0).fit(self.data)
-		mu = kmeans.cluster_centers_
-		labels = kmeans.labels_
-		mu = np.array(sorted(mu.tolist()))
-		labels = np.array(sorted(labels.tolist()))
+        covariances = np.array([np.eye(self.dataDimens) for i in range(self.n)])
 
+        return mu, covariances
 
-		covariances = np.array([np.eye(self.dataDimens) for i in range(self.n)])
+    def observationProb(self, mean, covars):
 
-		return mu,covariances
+        b = np.zeros((self.n, self.dataLength))
+        for i in range(self.n):
+            b[i, :] = multivariate_normal.pdf(self.data, mean[i], covars[i], allow_singular=True)
 
+        return np.log(b)
 
+    def forward(self, prior, a, b):
 
-	def observationProb(self,mean,covars):
+        alpha = np.ones((self.n, self.dataLength)) * np.log(0)
 
+        # Initialization Step
+        alpha[:, 0] = prior + b[:, 0]
 
-		b = np.zeros((self.n,self.dataLength))
-		for i in range(self.n):
-			b[i,:] = multivariate_normal.pdf(self.data,mean[i],covars[i], allow_singular=True)
+        # Recursion Step
+        for t in range(1, self.dataLength):
+            for j in range(0, self.n):
+                alpha[j, t] = b[j, t] + logsumexp(alpha[:, t - 1] + a[:, j])
 
-		return np.log(b)
+        return alpha
 
+    def forward_backward(self, prior, a, b):
+        # Initialization Step
 
+        beta = np.ones((self.n, self.dataLength)) * np.log(0)
+        alpha = np.ones((self.n, self.dataLength)) * np.log(0)
+        alpha[:, 0] = prior + b[:, 0]
+        beta[:, -1] = np.ones(self.n) * np.log(1)
 
-	def forward(self, prior, a, b):
+        for t in range(1, self.dataLength):
+            tbeta = self.dataLength - 1 - t
+            for j in range(self.n):
+                beta[:, tbeta] = np.logaddexp(beta[:, tbeta], a[:, j] + b[j, tbeta + 1] + beta[j, tbeta + 1])
+                tmp = 0
+                for i in range(self.n):
+                    tmp = np.logaddexp(tmp, alpha[i, t - 1] + a[i, j])
+                alpha[j, t] = b[j, t] + tmp
 
-		alpha = np.ones((self.n,self.dataLength))*np.log(0)
+        return alpha, beta
 
-		# Initialization Step
-		alpha[:,0] = prior+b[:,0]
+    def computeLikelihood(self, alpha):
+        likelihood = np.log(0)
+        for i in range(self.n):
+            likelihood = np.logaddexp(likelihood, alpha[i, -1])
+        return likelihood
 
-		# Recursion Step
-		for t in range(1,self.dataLength):
-			for j in range(0,self.n):
-				alpha[j,t] = b[j,t] + logsumexp(alpha[:,t-1]+a[:,j])
+    def backward(self, a, b):
 
+        beta = np.ones((self.n, self.dataLength)) * np.log(0)
 
-		return alpha
+        # Initialization Step
+        list2reverse = range(0, self.dataLength - 1)
+        list2reverse.reverse()
+        reversedList = list2reverse
 
-	def forward_backward(self, prior, a, b):
-		# Initialization Step
+        beta[:, -1] = np.ones(self.n) * np.log(1)
+        for t in reversedList:  # for each timestep except initialtime
+            for j in range(self.n):
+                beta[:, t] = np.logaddexp(beta[:, t], a[:, j] + b[j, t + 1] + beta[j, t + 1])
 
-		beta = np.ones((self.n,self.dataLength))*np.log(0)
-		alpha = np.ones((self.n,self.dataLength))*np.log(0)
-		alpha[:,0] = prior+b[:,0]
-		beta[:,-1] = np.ones(self.n)*np.log(1)
+        return beta
 
-		for t in range(1,self.dataLength):
-			tbeta = self.dataLength-1-t
-			for j in range(self.n):
-				beta[:,tbeta] = np.logaddexp( beta[:,tbeta] , a[:,j]  + b[j,tbeta+1]+   beta[j,tbeta+1])
-				tmp = 0
-				for i in range(self.n):
-					tmp =  np.logaddexp(tmp, alpha[i,t-1]+a[i,j])
-				alpha[j,t]  = b[j,t] + tmp
+    def Gamma(self, alpha, beta):
 
-		return alpha, beta
+        normalizer = logsumexp(alpha + beta, axis=0)
 
+        gamma = alpha + beta - normalizer
 
-	def computeLikelihood(self, alpha):
-		likelihood = np.log(0)
-		for i in range(self.n):
-			likelihood = np.logaddexp(likelihood, alpha[i,-1])
-		return likelihood
+        npt.assert_almost_equal(np.sum(np.exp(gamma), axis=0), 1.0)  # up to default decimal 7
 
+        return gamma
 
-	def backward(self, a, b):
+    def Zeta(self, alpha, a, beta, b):
 
-		beta = np.ones((self.n,self.dataLength))*np.log(0)
+        normalizer = np.ones(self.dataLength - 1) * np.log(0)
+        for i in range(self.n):
+            for j in range(self.n):
+                normalizer[:] = np.logaddexp(normalizer[:], \
+                                             alpha[i, :-1] + a[i, j] + b[j, 1:] + beta[j, 1:])
 
-		# Initialization Step
-		list2reverse = range(0,self.dataLength-1)
-		list2reverse.reverse(); reversedList = list2reverse
+        zeta_tensor = np.zeros((self.dataLength - 1, self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                zeta_tensor[:, i, j] = alpha[i, :-1] + a[i, j] + b[j, 1:] + beta[j, 1:] - normalizer[:]
+        # print "zeta_inside",zeta_tensor
+        return zeta_tensor
 
+    def updateTransition(self, zeta_tensor, gamma):
 
-		beta[:,-1] = np.ones(self.n)*np.log(1)
-		for t in reversedList: # for each timestep except initialtime
-			for j in range(self.n):
-				beta[:,t] = np.logaddexp( beta[:,t] , a[:,j]  + b[j,t+1]+   beta[j,t+1])
+        sum_zeta = logsumexp(zeta_tensor, axis=0)
+        sum_gamma = logsumexp(gamma[:, :-1], axis=1)
 
+        a = np.ones((self.n, self.n)) * np.log(0)
+        for i in range(self.n):
+            a[i, :] = sum_zeta[i, :] - sum_gamma[i]
 
+        # Check if sums to 1.
+        # npt.assert_almost_equal(np.sum(np.exp(a), axis = 1), 1.0)
+        return a
 
-		return beta
+    def updatePrior(self, gamma):
 
-	def Gamma(self,alpha,beta):
+        prior = gamma[:, 0]
+        npt.assert_almost_equal(np.sum(np.exp(prior)), 1.0)
 
+        return prior
 
-		normalizer = logsumexp(alpha +  beta, axis = 0)
+    def updateMean(self, gamma):
 
-		gamma = alpha + beta - normalizer
+        gamma = np.exp(gamma)
 
-		npt.assert_almost_equal(np.sum(np.exp(gamma), axis = 0), 1.0) # up to default decimal 7
+        mu = np.zeros((self.n, self.dataDimens))
+        for i in range(self.n):
 
-		return gamma
+            sum_nom = 0
+            sum_denom = 0
+            for t in range(self.dataLength):
+                sum_nom += gamma[i, t] * self.data[t, :]
+                sum_denom += gamma[i, t]
 
-	def Zeta(self,alpha, a, beta, b):
+            mu[i] = sum_nom / sum_denom
 
+        return mu
 
-		normalizer = np.ones(self.dataLength-1)*np.log(0)
-		for i in range(self.n):
-			for j in range(self.n):
-				normalizer[:] = np.logaddexp(normalizer[:], \
-						alpha[i,:-1]+a[i,j]+b[j,1:]+beta[j,1:])
+    def updateCovars(self, gamma, mean):
+        gamma = np.exp(gamma)
+        covariances = []
+        for i in range(self.n):
+            sum_nom = np.zeros(self.dataDimens)
+            sum_denom = 0
+            mu = mean[i].reshape(1, self.dataDimens)
+            for t in range(self.dataLength):
+                o = self.data[t, :].reshape(1, self.dataDimens)
+                sum_nom = sum_nom + gamma[i, t] * np.outer(o - mu, o - mu)
+                sum_denom = sum_denom + gamma[i, t]
 
+            covariances.append(sum_nom / sum_denom)
 
-		zeta_tensor = np.zeros((self.dataLength-1, self.n, self.n))
-		for i in range(self.n):
-			for j in range(self.n):
-				zeta_tensor[:,i,j] = alpha[i,:-1]+a[i,j]+b[j,1:]+beta[j,1:] -normalizer[:]
-		# print "zeta_inside",zeta_tensor
-		return zeta_tensor
+        return np.array(covariances)
 
-	def updateTransition(self,zeta_tensor,gamma):
+    def Baum_Welch(self, number_of_iterations=1000, minError=0.01):
+        # initialize all
+        prior = self.initPrior()
+        a = self.initTransition()
+        mu, sigma = self.initMeansCovs()
 
+        b = self.observationProb(mu, sigma)
+        likelihood = np.log(20)
+        oldLikelihood = np.log(10)
+        counter = 0
 
+        while np.abs(likelihood - oldLikelihood) / np.abs(oldLikelihood) > minError:
+            # Expectation Step
+            start = time()
+            alpha, beta = self.forward_backward(prior, a, b)
+            gamma = self.Gamma(alpha, beta)
+            zeta = self.Zeta(alpha, a, beta, b)
 
-		sum_zeta  = logsumexp(zeta_tensor, axis=0)
-		sum_gamma = logsumexp(gamma[:,:-1], axis = 1)
+            # Maximization Step
 
+            prior = self.updatePrior(gamma)
+            a = self.updateTransition(zeta, gamma)
+            mu = self.updateMean(gamma)
+            sigma = self.updateCovars(gamma, mu)
+            sigma = sigma + np.eye(self.dataDimens) * (10 ** -10)
+            b = self.observationProb(mu, sigma)
 
-		a = np.ones((self.n,self.n))*np.log(0)
-		for i in range(self.n):
-			a[i,:] = sum_zeta[i,:] - sum_gamma[i]
+            oldLikelihood = likelihood
+            likelihood = self.computeLikelihood(alpha)
 
-		# Check if sums to 1.
-		# npt.assert_almost_equal(np.sum(np.exp(a), axis = 1), 1.0)
-		return a
+            # print likelihood,counter
 
-	def updatePrior(self,gamma):
+            counter += 1
+        # if counter == 1:
+        #     break
+        # print "counter is :\n",counter, "\n"
 
-		prior = gamma[:,0]
-		npt.assert_almost_equal(np.sum(np.exp(prior)), 1.0)
-
-		return prior
-
-	def updateMean(self,gamma):
-
-		gamma = np.exp(gamma)
-
-		mu = np.zeros((self.n, self.dataDimens))
-		for i in range(self.n):
-
-			sum_nom = 0
-			sum_denom = 0
-			for t in range(self.dataLength):
-				sum_nom  +=  gamma[i,t]*self.data[t,:]
-				sum_denom +=  gamma[i,t]
-
-			mu[i] =sum_nom/sum_denom
-
-
-		return mu
-
-
-	def updateCovars(self,gamma,mean):
-		gamma = np.exp(gamma)
-		covariances = []
-		for i in range(self.n):
-			sum_nom = np.zeros(self.dataDimens)
-			sum_denom = 0
-			mu = mean[i].reshape(1,self.dataDimens)
-			for t in range(self.dataLength):
-				o  = self.data[t,:].reshape(1,self.dataDimens)
-				sum_nom   = sum_nom   + gamma[i,t]*np.outer(o-mu,o-mu)
-				sum_denom = sum_denom + gamma[i,t]
-
-			covariances.append(sum_nom/sum_denom)
-
-		return np.array(covariances)
-
-	def Baum_Welch(self,number_of_iterations = 1000,minError = 0.01):
-		# initialize all
-		prior     = self.initPrior()
-		a         = self.initTransition()
-		mu, sigma = self.initMeansCovs()
-	
-
-		b         = self.observationProb(mu, sigma)
-		likelihood    = np.log(20)
-		oldLikelihood = np.log(10)
-		counter = 0
-
-
-
-		while np.abs(likelihood-oldLikelihood)/np.abs(oldLikelihood) > minError:
-
-
-			# Expectation Step
-			start = time()
-			alpha, beta = self.forward_backward(prior, a, b)
-			gamma = self.Gamma(alpha,beta)
-			zeta  = self.Zeta(alpha, a, beta, b)
-
-			# Maximization Step
-
-			prior = self.updatePrior(gamma)
-			a     = self.updateTransition(zeta, gamma)
-			mu    = self.updateMean(gamma)
-			sigma = self.updateCovars(gamma,mu)
-			sigma = sigma + np.eye(self.dataDimens)*(10**-10)
-			b     = self.observationProb(mu,sigma)
-
-
-			oldLikelihood = likelihood
-			likelihood    = self.computeLikelihood(alpha)
-
-			#print likelihood,counter
-
-			counter += 1
-			# if counter == 1:
-			#     break
-			# print "counter is :\n",counter, "\n"
-
-		return prior,a,mu,sigma, likelihood
+        return prior, a, mu, sigma, likelihood
